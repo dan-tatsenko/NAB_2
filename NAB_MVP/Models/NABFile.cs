ae58f3ffae4a425da6d0a5079f4bdafcc8c1a847
Dan Tatsenko <danil.tatsenko@gmail.com>
2019-05-03 00:56:06 +1200

    Correcting paths

---
+++ NAB_MVP/Models/NABFile.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NAB_MVC.Models
+{
+    public class NABFile : IBankingFile
+    {
+        private List<Transaction> TransactionList;
+        private bool CurrentTransactionSaved;
+        private int CurrentTransactionIndex;
+        public int Count => TransactionList.Count;
+
+        public NABFile()
+        {
+            TransactionList = new List<Transaction>();
+            CurrentTransactionIndex = -1;
+        }
+
+        public Transaction this[int index]
+        {
+            get => TransactionList[index];
+            set => TransactionList[index] = value;
+        }
+
+        public event EventHandler TransactionAdded;
+        public event EventHandler TransactionDeleted;
+        public event EventHandler TransactionChanged;
+
+        public void Add(Transaction transaction)
+        {
+            TransactionList.Add(transaction);
+            TransactionAdded(this, new EventArgs());
+            CurrentTransactionSaved = false;
+        }
+
+        public List<string> ExportToList()
+        {
+            List<string> result = new List<string>();
+            foreach (var item in TransactionList)
+            {
+                result.Add(item.ToString());
+            }
+            return result;
+        }
+
+        public void Remove(int index)
+        {
+            if (index>=0 && index<Count)
+            {
+                TransactionList.RemoveAt(index);
+                TransactionDeleted(this, new EventArgs());
+            }
+        }
+
+        public void SaveToFile(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal TotalTransactionAmount()
+        {
+            decimal r = 0;
+            foreach (var t in TransactionList)
+            {
+                r += t.Amount;
+            }
+            return r;
+        }
+
+        public List<string> GetPaymentInstructionsList()
+        {
+            List<string> list = new List<string>();
+            list.Add("05 - Payment");
+            list.Add("25 - Refund");
+            list.Add("35 - Chargeback");
+            return list;
+        }
+
+        public List<string> GetPaymentChannelsList()
+        {
+            List<string> list = new List<string>();
+            list.Add("PBW - WEB Payments");
+            list.Add("IVR - Interactive Voice Response");
+            list.Add("BPY - Bank Payment");
+            list.Add("DPP - Direct Post Payment");
+            return list;
+        }
+    }
+}
---
+++ NAB_MVP/Models/Transaction.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NAB_MVC.Views;
+
+namespace NAB_MVC.Models
+{
+    public enum PaymentInstructionType
+    {
+        Payment,            //05 - Payment
+        ErrorCorrection,    //25 - Refund
+        Reversal            //35 - Chargeback
+    }
+
+   public enum PaymentChannelType
+    {
+        WebPayment,                 //PBW - WEB Payments
+        InteractiveVoiceResponse,   //IVR - Interactive Voice Response
+        BankPayment,                //BPY - Bank Payment
+        DirectPostPayment,          //DPP - Direct Post Payment
+        PayInPerson                 //PiP - Payment in Person
+    }
+
+    public class Transaction : ITransaction
+    {
+        public Transaction()
+        {
+            _recordType = "50";
+        }
+
+        public event EventHandler<EventArgs> AmountChanged;
+
+        private decimal _amount;
+        private string _recordType;
+
+        public string RecordType { get => _recordType; }
+
+        public string SourceIdentifier { get => SourceIdentifier; set => value.Take(10); }
+        public string AccountNumber { get; set; }
+        
+        public PaymentInstructionType PaymentInstruction { get; set; }
+        public PaymentChannelType PaymentChannel { get; set; }
+        public string MaskedCreditCard { get; set; }
+        public byte ErrorCorrectionReason { get; set; }
+        public decimal Amount
+        {
+            get
+            {
+                return _amount;
+            }
+
+            set
+            {
+                _amount = value;
+                AmountChanged(this, new EventArgs());
+            }
+        }
+        public DateTime PaymentDateTime { get; set; }
+        public DateTime SettlementDate { get; set; }
+        public string BankTransactionID { get; set; }
+        public string AuthorisationCode { get; set; }
+        public string OriginalReference { get; set; }
+
+        public string PaymentInstructionCode { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public override string ToString()
+        {
+            string r = "";
+
+            r += _recordType;
+            return r;
+        }
+    }
+}
